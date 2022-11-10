0a163afb94ae33ff8d720acc490fd0c5c3bd0b09
Gabriel Barth <gabrielbarth@Gabriels-MacBook-Pro.local>
2022-11-09 21:24:34 -0400

    :lipstick: style: adds app theme

--- App.tsx
+++ App.tsx
@@ -7,6 +7,9 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,12 +17,13 @@ export default function App() {
   if (!fontsLoaded) return;
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
+      <Loading />
     </NativeBaseProvider>
   );
 }
