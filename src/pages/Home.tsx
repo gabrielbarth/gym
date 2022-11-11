6e297250012ca3b24fc5492cb5b59e096c1c4366
Gabriel Barth <gabrielbarth@Gabriels-MacBook-Pro.local>
2022-11-10 22:04:08 -0400

    :sparkles: feat: adds Group (workout muscle) component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,32 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+import { HStack, VStack, FlatList } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export function Home() {
+  const [groups, setGroups] = useState(["costa", "ombro", "bíceps", "tríceps"]);
+  const [groupSelected, setGroupSelected] = useState("costa");
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            onPress={() => setGroupSelected(item)}
+            isActive={groupSelected === item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
