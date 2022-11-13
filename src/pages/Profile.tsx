6e617f5a2e9abe473aa9c1c0a695eaf31512c5f7
Gabriel Barth <gabrielbarth@Gabriels-MacBook-Pro.local>
2022-11-12 22:08:47 -0400

    :sparkles: feat: adds UserPhoto compoennt and its Skeleton

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,49 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Center, ScrollView, VStack, Skeleton, Text } from "native-base";
+import { TouchableOpacity } from "react-native";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/gabrielbarth.png" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
