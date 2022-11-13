60606f318bf5351fc23a37ed5befae4c018d6a28
Gabriel Barth <gabrielbarth@Gabriels-MacBook-Pro.local>
2022-11-12 21:43:18 -0400

    :sparkles: feat: adds HistoryCard component

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,10 +1,16 @@
-import { Center, VStack } from "native-base";
+import { VStack } from "native-base";
+
 import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
